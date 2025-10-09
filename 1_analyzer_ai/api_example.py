c99050faef4246dd64049cd77000ecad53d63509
Xxxyxxx <bezekyigit0@gmail.com>
2025-10-09 21:19:59 +0300

    continuation on the chabot antrhpic api

--- 1_analyzer_ai/api_example.py
+++ 1_analyzer_ai/api_example.py
@@ -27,12 +27,29 @@ else:
         
         
     )
-    msg=message.content
+    msg=message
     
 if __name__=="__main__":
     try:
-        print(msg)
-        print(sys.argv)
+        print(f""" 1 learnign tool we will gain out of here is the inspectino tools of python 
+              \n 1- dir(xxx) this shows us all the available methods and attribiutes that is available on a object
+              \n kind of asking  what can i do with this object?
+              \n 2- type(xxx) this is tells us what class or type an object is
+              \n such as type(msg) => {type(msg)} will give us string if the antrhopic comm is not 
+              ebtablished if so then antroppiuc type message should it be and type(int)  => {type(int)} will give us ttpye
+               lets print the msg -> {msg}
+              \n then lets print the tpye of it {type(msg)}
+              \n lets also see the type of its content {type(msg.content)} \n
+              \n {{dir()}}, {dir()}
+              \n now we will the  {{dir(msg)}}, {dir(msg)} \n 
+              \nand then {{dir(msg.content)}} => {dir(msg.content)}\n
+                         
+              
+              
+              \n
+               
+              """)
+        
     except:
         print("it failed")
   
--- 1_analyzer_ai/sys_module_ex.py
+++ 1_analyzer_ai/sys_module_ex.py
@@ -36,11 +36,21 @@ user_input= sys.stdin.readline()
 user_input_2= input()
 print(f"the first time variation retrieves the innupt as {user_input} and the secnond nput is {user_input_2}{"\n"*5} if oyu see that it got wrong please enter q if correct just proceed")
 user_input_verify = sys.stdin.readline()
-if user_input_verify == 'q':
+if user_input_verify.strip() == 'q':
     print(f"the system failed to aquire the right inpiuts so we are exiting with the call of exit in the sys module sys.exit()")
     sys.exit(1)
 else:
     print(f"nice that the system was succesful in the proces if aquirign using the sys module")
     
-print(f"now we can proceed with stdout")
+print(f"""now we can proceed with stdout
+      \n ok now we will try the sys.strdout.write comand\n
+      """)
+print(sys.stdout.write(f"\n hey do you see. this mesage{"wo "*5}"))
+print(f"now you are probably wondering why there is a nuber right it is because the two thiungs being happened it steraming the string bundle to screen and since it is wrapped aptuna. print statemnt the ...write returns the total chars it writes so that also i s printed \n so the wirte usage would be out tof the print wrapper")
+sys.stdout.write(f"now do oyu see any numbers?\n")
 
+sys.stdout.write(f"""no right o the thing is that our sys.stdout determines where to flush the stream
+                 \n so lets see our current sys.stdout():{sys.stdout}
+                 
+                 """)
+# now we are taking pause on sys exploriung back to main project of the cahtbot learning we wil continenue on sys and flushing out on a file and so on next 
