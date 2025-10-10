49171cbda30539b63249d00973eba260a3b77c51
Xxxyxxx <bezekyigit0@gmail.com>
2025-10-10 17:19:23 +0300

    some rand changes

--- 1_analyzer_ai/api_example.py
+++ 1_analyzer_ai/api_example.py
@@ -28,31 +28,60 @@ else:
         
     )
     msg=message
+def send_to_cli(msg_to_cli):
+    if api_key is None:
+        print(f" please enter the api key")
+    else:
+        client = anthropic.Anthropic(api_key= os.getenv("ANTHROPIC_API_KEY"))
+        message = client.messages.create(
+            model="claude-sonnet-4-5-20250929",
+            max_tokens=4024,
+            messages=[
+                {
+                "role":"user",
+                "content":msg_to_cli
+                
+            }
+                ]
+            
+        )
+        return message.content[0].text
     
 if __name__=="__main__":
-    try:
-        print(f""" 1 learnign tool we will gain out of here is the inspectino tools of python 
-              \n 1- dir(xxx) this shows us all the available methods and attribiutes that is available on a object
-              \n kind of asking  what can i do with this object?
-              \n 2- type(xxx) this is tells us what class or type an object is
-              \n such as type(msg) => {type(msg)} will give us string if the antrhopic comm is not 
-              ebtablished if so then antroppiuc type message should it be and type(int)  => {type(int)} will give us ttpye
-              \n 2.1 > also lets try one interesting thing and see what is going to happen {type(type(int))}this interesting thig gives us this {type(type(int))} \n
-              \n as of also to see something else because we just observeed teh same thing as single type of an int so lets try ewith the message
-              \n 1 type -> {type(msg)} , then 2 type {type(type(msg))} maybe also 3 types {type(type(type(msg)))}
-               \nlets print the msg -> {msg}
-              \n then lets print the tpye of it {type(msg)}
-              \n lets also see the type of its content {type(msg.content)} \n
-              \n {{dir()}}, {dir()}
-              \n now we will the  {{dir(msg)}}, {dir(msg)} \n 
-              \nand then {{dir(msg.content)}} => {dir(msg.content)}\n
+    inn="0"
+    while inn!="q":
+        if inn == '0':
+            print(f"please enter whatever you want to ask to the chatbot adn you end the converstation by jsut pressing q and enter")
+            
+        inn = sys.stdin.readline()
+        print(inn)
+        print(send_to_cli(inn))
+        
+    # for i in msg.content[0].text:
+    #     print(i)
+    # try:
+    #     print(f""" 1 learnign tool we will gain out of here is the inspectino tools of python 
+    #           \n 1- dir(xxx) this shows us all the available methods and attribiutes that is available on a object
+    #           \n kind of asking  what can i do with this object?
+    #           \n 2- type(xxx) this is tells us what class or type an object is
+    #           \n such as type(msg) => {type(msg)} will give us string if the antrhopic comm is not 
+    #           ebtablished if so then antroppiuc type message should it be and type(int)  => {type(int)} will give us ttpye
+    #           \n 2.1 > also lets try one interesting thing and see what is going to happen {type(type(int))}this interesting thig gives us this {type(type(int))} \n
+    #           \n as of also to see something else because we just observeed teh same thing as single type of an int so lets try ewith the message
+    #           \n 1 type -> {type(msg)} , then 2 type {type(type(msg))} maybe also 3 types {type(type(type(msg)))}
+    #            \nlets print the msg -> {msg}
+    #           \n then lets print the tpye of it {type(msg)}
+    #           \n lets also see the type of its content {type(msg.content)} \n
+    #           \n {{dir()}}, {dir()}
+    #           \n now we will the  {{dir(msg)}}, {dir(msg)} \n 
+    #           \nand then {{dir(msg.content)}} => {dir(msg.content)}\n
                          
               
               
-              \n
+    #           \n
                
-              """)
+    #           """)
         
-    except:
-        print("it failed")
+    # except:
+    #     print("it failed")
   
--- 1_analyzer_ai/core_inspect_piton.py
+++ 1_analyzer_ai/core_inspect_piton.py
@@ -1,2 +1,20 @@
+"""hi this is isnpector tools for python"""
 x= [1,2,3]
 print(dir(x))
+attr_of_empty = x.__getattribute__
+print(f"""
+      
+     1-now empty 
+      {dir()}  
+      
+      2- now the none just to see if the none and emoty are same
+      {dir(None)}
+      
+      3- lets try something form the empty dir run and to see whats that
+      {__package__}
+      {__annotations__}
+      {__builtins__}
+      {__doc__}
+      {type(__annotations__)}
+      
+      """)
